536d72670e056beb97dfec54e4eb5dd4d97454db
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-10-18 14:23:55 +0900

    fix

--- infra/terraform/modules/artifact_registry/outputs.tf
+++ infra/terraform/modules/artifact_registry/outputs.tf
@@ -1,6 +1,7 @@
 output "repository" {
   description = "Full resource name of the Artifact Registry repository."
-  value = var.manage_repository ?
-    google_artifact_registry_repository.repository[0].name :
-    data.google_artifact_registry_repository.existing[0].name
+  value = coalesce(
+    try(google_artifact_registry_repository.repository[0].name, null),
+    try(data.google_artifact_registry_repository.existing[0].name, null)
+  )
 }
